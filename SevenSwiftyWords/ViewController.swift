a60ed739621785d44f8712968d8dd8746c924593
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-08-06 18:10:09 -0300

    Initial Commit

---
+++ SevenSwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenSwiftyWords
+//
+//  Created by Linconl Rufino on 06/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
